6a0b995e93d916e89fcc6820ff099c4e7e140fd6
Hamza <mlearning51@gmail.com>
2025-03-04 16:49:42 +0500

    Create check_pandas.py

---
+++ models/check_pandas.py
@@ -0,0 +1,17 @@
+from odoo import models, api
+from odoo.exceptions import UserError
+
+class CheckPandasModule(models.AbstractModel):
+    _name = 'check.pandas.module'
+    _description = 'Check Pandas Module Before Installation'
+
+    @api.model
+    def _check_pandas_installed(self):
+        try:
+            import pandas  # noqa: F401
+        except ImportError:
+            raise UserError("The 'pandas' Python module is not installed. Please install it using `pip install pandas` before installing this module.")
+
+    @api.model
+    def _register_hook(self):
+        self._check_pandas_installed()
